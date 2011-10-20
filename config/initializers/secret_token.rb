69ca67ea317a86734bf6bd6b6a46a3f1c04acecf
Lars Gollnow <misc@mfind.de>
2011-10-20 16:31:55 +0200

    * fresh rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Fest2::Application.config.secret_token = '7e87895aec53a0fc5d3539a717b331f59a0860ec85da44344afd21e1a5e47a3a345edc5e382570f3b8a9f4841efe6c25235a87ba349d7ec4b7a2d5aa65def932'
